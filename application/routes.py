9dfc30dc6c6673204081bc9727b8f415acfcf66e
Duong Thanh Hiep <thanhhiep0705@gmail.com>
2023-05-31 11:04:58 +0700

    Update code

--- application/routes.py
+++ application/routes.py
@@ -70,11 +70,303 @@ def test():
 
 @app.route('/data')
 def data():
-    return render_template('data.html')
+    source_data = [
+        {
+            'title' : 'Job Postings Data',
+            'data' : [
+                {
+                    'title': 'LinkedIn',
+                    'information':[
+                        {
+                            'title': 'Members',
+                            'value': '740+ M'
+                        },
+                        {
+                            'title': 'Companies',
+                            'value': '50+ M'
+                        },
+                        {
+                            'title': 'Languages',
+                            'value': '24'
+                        }
+                    ],
+                    'website': 'https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0'
+                },
+                {
+                    'title': 'NodeFlair',
+                    'information':[
+                        {
+                            'title': 'Members',
+                            'value': '4,000+'
+                        },
+                        {
+                            'title': 'Companies',
+                            'value': '5,000+'
+                        },
+                        {
+                            'title': 'Partners',
+                            'value': '200+'
+                        }
+                    ],
+                    'website': 'https://nodeflair.com/'
+                }
+            ]
+        },
+
+        {
+            'title' : 'Courses Data',
+        
+            'data' : [
+                {
+                    'title': 'Coursera',
+                    'information':[
+                        {
+                            'title': 'Courses',
+                            'value': '4,000+'
+                        },
+                        {
+                            'title': 'Learners',
+                            'value': '82+ M'
+                        },
+                        {
+                            'title': 'Partners',
+                            'value': '200+'
+                        }
+                    ],
+                    'website': 'https://www.coursera.org/?irclickid=wlvRjDSqHxyNUeaU9XwM0VgNUkASXR1pbw4DUw0&irgwc=1&utm_medium=partners&utm_source=impact&utm_campaign=2985301&utm_content=b2c'
+                },
+                {
+                    'title': 'Udemy',
+                    'information':[
+                        {
+                            'title': 'Courses',
+                            'value': '130,000+'
+                        },
+                        {
+                            'title': 'Learners',
+                            'value': '40+ M'
+                        },
+                        {
+                            'title': 'Countries',
+                            'value': '190+'
+                        }
+                    ],
+                    'website': 'https://www.udemy.com/?ranMID=39197&ranEAID=yNfEamYSgXk&ranSiteID=yNfEamYSgXk-Q716xy3YHMHwt8IcQqpOnA&LSNPUBID=yNfEamYSgXk&utm_source=aff-campaign&utm_medium=udemyads&gclid=Cj0KCQjwyLGjBhDKARIsAFRNgW8Ng4w4tvTFvPbBKcXsbBwCk4SyZdluQ97e3dvxkn5X7DtUWHJqyQAaAv0KEALw_wcB'
+                },
+                {
+                    'title': 'edX',
+                    'information':[
+                        {
+                            'title': 'Courses',
+                            'value': '3,000+'
+                        },
+                        {
+                            'title': 'Learners',
+                            'value': '38+ M'
+                        },
+                        {
+                            'title': 'Partners',
+                            'value': '120+'
+                        }
+                    ],
+                    'website': 'https://www.edx.org/?irclickid=wlvRjDSqHxyNUeaU9XwM0VgNUkASXW0Zbw4DUw0&utm_source=affiliate&utm_medium=Ecom%20EWAY&utm_campaign=Online%20Tracking%20Link_&utm_content=ONLINE_TRACKING_LINK&irgwc=1'
+                }
+            ]
+        }
+    ]
+    return render_template('data.html', source_data = source_data)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    team_members = [
+        {
+            'name': 'Nguyen Tran Minh Thu',
+            'role': 'Instructor',
+            'image': '../static/img/businesswoman.png',
+            'information':[
+                {
+                    'title': 'University',
+                    'value': 'University of Science'
+                },
+                {
+                    'title': 'Degree',
+                    'value': 'Doctor'
+                },
+                {
+                    'title': 'Job',
+                    'value': 'Lecturers'
+                },
+                {
+                    'title': 'Major',
+                    'value': 'Information System'
+                },
+                {
+                    'title': 'Email',
+                    'value': 'ntmthu@fit.hcmus.edu.vn'
+                },
+                {
+                    'title': 'Hobbies',
+                    'value': 'Reading, Cooking'
+                }
+            ]
+        },
+        {
+            'name': 'Nguyen Pham Quang Dung',
+            'role': 'Team Leader',
+            'image': '../static/img/man.png',
+            'information':[
+                {
+                    'title': 'University',
+                    'value': 'University of Science'
+                },
+                {
+                    'title': 'StudentID',
+                    'value': '19120485'
+                },
+                {
+                    'title': 'Major',
+                    'value': 'Data Science'
+                },
+                {
+                    'title': 'Email',
+                    'value': 'npqdung17@gmail.com'
+                },
+                {
+                    'title': 'Future Job',
+                    'value': 'Data Engineer'
+                },
+                {
+                    'title': 'Hobbies',
+                    'value': 'Hiking, Cooking'
+                }
+            ]
+        },
+        {
+            'name': 'Duong Thanh Hiep',
+            'role': 'Team Member',
+            'image': '../static/img/hacker.png',
+            'information':[
+                {
+                    'title': 'University',
+                    'value': 'University of Science'
+                },
+                {
+                    'title': 'StudentID',
+                    'value': '19120505'
+                },
+                {
+                    'title': 'Major',
+                    'value': 'Computer Science'
+                },
+                {
+                    'title': 'Email',
+                    'value': 'hiep0705@gmail.com'
+                },
+                {
+                    'title': 'Future Job',
+                    'value': 'Web Developer'
+                },
+                {
+                    'title': 'Hobbies',
+                    'value': 'Traveling, Football'
+                }
+            ]
+        },
+        {
+            'name': 'Nguyen Thi Tieu Mi',
+            'role': 'Team Member',
+            'image': '../static/img/woman.png',
+            'information':[
+                {
+                    'title': 'University',
+                    'value': 'University of Science'
+                },
+                {
+                    'title': 'StudentID',
+                    'value': '19120577'
+                },
+                {
+                    'title': 'Major',
+                    'value': 'Information System'
+                },
+                {
+                    'title': 'Email',
+                    'value': 'tieumi2509@gmail.com'
+                },
+                {
+                    'title': 'Future Job',
+                    'value': 'Web Developer'
+                },
+                {
+                    'title': 'Hobbies',
+                    'value': 'Reading, Cooking'
+                }
+            ]
+        },
+        {
+            'name': 'Le Thanh Loc',
+            'role': 'Team Member',
+            'image': '../static/img/businessman.png',
+            'information':[
+                {
+                    'title': 'University',
+                    'value': 'University of Science'
+                },
+                {
+                    'title': 'StudentID',
+                    'value': '19120562'
+                },
+                {
+                    'title': 'Major',
+                    'value': 'Information System'
+                },
+                {
+                    'title': 'Email',
+                    'value': 'lochcmus@gmail.com'
+                },
+                {
+                    'title': 'Future Job',
+                    'value': 'Data Engineer'
+                },
+                {
+                    'title': 'Hobbies',
+                    'value': 'Singing, Traveling'
+                }
+            ]
+        },
+        {
+            'name': 'Nguyen Thi Kim Ngan',
+            'role': 'Team Member',
+            'image': '../static/img/girl.png',
+            'information':[
+                {
+                    'title': 'University',
+                    'value': 'University of Science'
+                },
+                {
+                    'title': 'StudentID',
+                    'value': '19120598'
+                },
+                {
+                    'title': 'Major',
+                    'value': 'Information System'
+                },
+                {
+                    'title': 'Email',
+                    'value': 'ntkn.mnkt@gmail.com'
+                },
+                {
+                    'title': 'Future Job',
+                    'value': 'Web Developer'
+                },
+                {
+                    'title': 'Hobbies',
+                    'value': 'Reading, Hiking'
+                }
+            ]
+        }
+    ]
+    return render_template('about.html', team_members=team_members)
 
 @app.route('/course')
 def course():
