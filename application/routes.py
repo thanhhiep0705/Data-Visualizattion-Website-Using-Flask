762ff67622fcaae712e24086dc7e93a5913e3dfc
Duong Thanh Hiep <thanhhiep0705@gmail.com>
2023-06-04 22:53:24 +0700

    Update UI

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
+import os
 from application import app
-from flask import render_template
+from flask import render_template, url_for
 import pandas as pd
 import json
 import plotly
@@ -14,7 +15,7 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/course-crawler')
+@app.route('/manage-crawler')
 def test():
 # Dữ liệu mẫu
     x_data = [1, 2, 3, 4, 5]
@@ -54,7 +55,7 @@ def test():
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     # Hiển thị biểu đồ
-    return render_template('manage-course-crawler.html', graphJSON = graphJSON)
+    return render_template('manage-crawler.html', graphJSON = graphJSON)
 
 @app.route('/data')
 def data():
@@ -356,8 +357,40 @@ def about():
     ]
     return render_template('about.html', team_members=team_members)
 
-@app.route('/course')
-def course():
+@app.route('/course-finder')
+def courseSearch():
+    # csv_url = url_for('static', filename='Course.csv')
+    # df = pd.read_csv(csv_url)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the file path for the CSV file
+    csv_path = os.path.join(current_dir, 'static', 'Course.csv')
+
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv(csv_path, encoding='latin1')
+    course = df.head(5)
+    top_course = df.head(5)
+
+    return render_template('course-finder.html', course = course , top_course = top_course)
+
+@app.route('/job-finder')
+def jobSearch():
+    # csv_url = url_for('static', filename='Course.csv')
+    # df = pd.read_csv(csv_url)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the file path for the CSV file
+    csv_path = os.path.join(current_dir, 'static', 'Job.csv')
+
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv(csv_path, encoding='latin1')
+    job = df.head(5)
+    top_job = df.head(5)
+
+    return render_template('job-finder.html', job = job , top_job = top_job)
+
+@app.route('/course-visualization')
+def courseVisualization():
     # Graph One
     df = px.data.medals_wide()
     fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"])
@@ -402,11 +435,10 @@ def course():
         },
     ]
 
+    return render_template('course-visualization.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON, graph4JSON=graph4JSON, graphsList = graphsList)
 
-    return render_template('course.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON, graph4JSON=graph4JSON, graphsList = graphsList)
-
-@app.route('/job-posting')
-def recruitment():
+@app.route('/job-visualization')
+def jobVisualization():
     # Graph One
     df = px.data.medals_wide()
     fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"])
@@ -428,5 +460,27 @@ def recruitment():
     fig4 = px.bar(data_canada, x='year', y='pop')
     graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
 
+    graphsList = [
+        {
+            'name':'Chart 1',
+            'chartId':'chart1',
+            'dialogId': 'dialog-chart1'
+        },
+        {
+            'name':'Chart 2',
+            'chartId':'chart2',
+            'dialogId': 'dialog-chart2'
+        },
+        {
+            'name':'Chart 3',
+            'chartId':'chart3',
+            'dialogId': 'dialog-chart3'
+        },
+        {
+            'name':'Chart 4',
+            'chartId':'chart4',
+            'dialogId': 'dialog-chart4'
+        },
+    ]
 
-    return render_template('job-posting.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON, graph4JSON=graph4JSON)
+    return render_template('job-visualization.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON, graph4JSON=graph4JSON, graphsList = graphsList)
