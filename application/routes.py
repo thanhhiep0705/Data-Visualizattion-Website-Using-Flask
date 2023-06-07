cae485dd5d512050eea2b3c4113a2d9cdc60c8ca
ThanhHiep75 <hiep.duong@goldeneyetech.com.vn>
2023-06-07 17:03:28 +0700

    Visualize course data

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
 import os
+import re
 from application import app
 from flask import render_template, request
 import pandas as pd
@@ -422,51 +423,121 @@ def jobSearch():
 
 @app.route('/course-visualization')
 def courseVisualization():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the file path for the CSV file
+    csv_path = os.path.join(current_dir, 'static', 'Course.csv')
+    df = pd.read_csv(csv_path, encoding='latin1')
     # Graph One
-    df = px.data.medals_wide()
-    fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"])
+    
+    fig1, fig1_dialog = graph1(df)
     graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+    graph1_dialogJSON = json.dumps(fig1_dialog, cls=plotly.utils.PlotlyJSONEncoder)
 
     # Graph two
-    df = px.data.iris()
-    fig2 = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
-              color='species')
+    fig2, fig2_dialog = graph2(df)
     graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    graph2_dialogJSON = json.dumps(fig2_dialog, cls=plotly.utils.PlotlyJSONEncoder)
 
     # Graph three
-    df = px.data.gapminder().query("continent=='Oceania'")
-    fig3 = px.line(df, x="year", y="lifeExp", color='country')
+    fig3, fig3_dialog = graph3(df)
     graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+    graph3_dialogJSON = json.dumps(fig3_dialog, cls=plotly.utils.PlotlyJSONEncoder)
 
-    #Graph four
-    data_canada = px.data.gapminder().query("country == 'Canada'")
-    fig4 = px.bar(data_canada, x='year', y='pop')
+    fig4, fig4_dialog = graph4(df)
     graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+    graph4_dialogJSON = json.dumps(fig4_dialog, cls=plotly.utils.PlotlyJSONEncoder)
 
-    graphsList = [
+    graphsData = [
         {
             'name':'Chart 1',
             'chartId':'chart1',
-            'dialogId': 'dialog-chart1'
+            'dialogId': 'dialog-chart1',
+            'graphData': graph1JSON,
+            'graphDialogData': graph1_dialogJSON
         },
         {
             'name':'Chart 2',
             'chartId':'chart2',
-            'dialogId': 'dialog-chart2'
+            'dialogId': 'dialog-chart2',
+            'graphData': graph2JSON,
+            'graphDialogData': graph2_dialogJSON
         },
         {
             'name':'Chart 3',
             'chartId':'chart3',
-            'dialogId': 'dialog-chart3'
+            'dialogId': 'dialog-chart3',
+            'graphData': graph3JSON,
+            'graphDialogData': graph3_dialogJSON
         },
-        {
+                {
             'name':'Chart 4',
             'chartId':'chart4',
-            'dialogId': 'dialog-chart4'
-        },
+            'dialogId': 'dialog-chart4',
+            'graphData': graph4JSON,
+            'graphDialogData': graph4_dialogJSON
+        }
     ]
+    return render_template('course-visualization.html', graphsData=graphsData)
 
-    return render_template('course-visualization.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON, graph4JSON=graph4JSON, graphsList = graphsList)
+def convert_to_hours(time):
+    match = re.match(r'(\d+(?:\.\d+)?)', str(time))
+    if match:
+        return float(match.group(1))
+    return None
+
+def convert_to_fee(fee):
+    if isinstance(fee, str):
+        match = re.search(r'\d+\.\d+|\d+', fee)
+        if match:
+            return float(match.group())
+    return 0
+
+def graph1(df):
+    new_df = df.loc[:, ['Enroll', 'Subject']]
+    new_df['Enroll'] = new_df['Enroll'].apply(convert_to_hours)
+    df_subject_enroll = new_df.groupby('Subject')['Enroll'].sum().reset_index()
+    df_subject_enroll.columns = ['Subject', 'Total Enroll']
+    fig = px.pie(df_subject_enroll, names="Subject", values="Total Enroll", height= 400, width= 340)
+    fig.update_layout(height= 420, width= 420, showlegend=False, margin=dict(t=35, l=0))
+    fig.update_traces(textposition='inside', textinfo='percent')
+    fig_dialog = px.pie(df_subject_enroll, names="Subject", values="Total Enroll")
+    fig_dialog.update_layout(height= 1100, width= 1100, legend=dict(orientation = "h", yanchor="bottom",y=-1.1,xanchor="left", x=0), margin=dict(l=275, r=275, t=15, b=15))
+    fig_dialog.update_traces(textposition='inside', textinfo='percent+label')
+    return fig, fig_dialog
+
+def graph2(df):
+    new_df = df.loc[:, ['Time', 'Subject']]
+    new_df['Time'] = new_df['Time'].apply(convert_to_hours)
+    df_subject_time = new_df.groupby('Subject')['Time'].mean().reset_index()
+    df_subject_time.columns = ['Subject', 'Average Time']
+    # subjects_to_plot = df_subject_time['Subject'].unique()[10:20]
+    # df_subject_time = df_subject_time.query('Subject in @subjects_to_plot')
+    fig = px.bar(df_subject_time, x="Subject", y="Average Time", color='Subject')
+    fig.update_layout(height=450, width=1080, xaxis={'visible': False}, margin=dict(t=0, l=0))
+    fig_dialog = px.bar(df_subject_time, x="Subject", y="Average Time", color='Subject')
+    fig_dialog.update_layout(height = 700, width =1000, xaxis={'visible': False, 'showticklabels': True, }, margin=dict(l=20, r=20, t=50, b=20), font=dict(size=10),legend=dict(orientation = "h", yanchor="bottom",y=-1.1,xanchor="left", x=0))
+    return fig, fig_dialog
+
+def graph3(df):
+    new_df = df.loc[:, ['Time', 'Level', 'Subject']]
+    new_df['Time'] = new_df['Time'].apply(convert_to_hours)
+    new_df = new_df.groupby(['Subject', 'Level'])['Time'].mean().reset_index()
+    fig = px.bar(new_df, x='Subject', y='Time', color='Level', barmode='stack')
+    fig.update_layout(height=450, width=450, xaxis={'visible': False, 'showticklabels': True}, margin=dict(t=0, l=0))
+    fig_dialog = px.bar(new_df, x='Subject', y='Time', color='Level', barmode='stack')
+    fig_dialog.update_layout(height=1200, width=1100, margin=dict(l=0,r=0, t=50, b=15))
+    return fig, fig_dialog
+
+def graph4(df):
+    new_df = df.loc[:, ['Fee', 'Level', 'Subject']]
+    new_df['Fee'] = new_df['Fee'].apply(convert_to_fee)
+    new_df = new_df.groupby(['Subject', 'Level'])['Fee'].sum().reset_index()
+    fig = px.bar(new_df, x='Subject', y='Fee', color='Level', barmode='stack')
+    fig.update_layout(height=450, width=450, xaxis={'visible': False, 'showticklabels': True}, margin=dict(t=0, l=0))
+    fig_dialog = px.bar(new_df, x='Subject', y='Fee', color='Level', barmode='stack')
+    fig_dialog.update_layout(height=1200, width=1100, margin=dict(l=0,r=0, t=50, b=15))
+    return fig, fig_dialog
 
 @app.route('/job-visualization')
 def jobVisualization():
@@ -515,3 +586,5 @@ def jobVisualization():
     ]
 
     return render_template('job-visualization.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON, graph4JSON=graph4JSON, graphsList = graphsList)
+
+
